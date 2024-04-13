dc670285ed3671f495d2f2e7b355939c034ab116
furan0 <augustin.delecroix@gmail.com>
2024-04-13 10:47:29 +0200

    Character & wololo scenes added

--- scenes/characters/Character.gd
+++ scenes/characters/Character.gd
@@ -1,13 +1,29 @@
 extends CharacterBody2D
 class_name Character
 
+## Handle lifecyle
 var is_dead := false
+signal dead()
+
+## Faction handling 
+enum EFaction {BLUE, RED, GAIA}
+## Listen to this player input 
+@export var faction : EFaction = EFaction.GAIA
+signal factionChanged()
 
 #Child nodes
 @onready var move_handler : MoveHandler = %MoveHandler
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Handle movement
 	velocity = move_handler.calculatedVelocity
 	move_and_slide()
+	
+
+func setFaction(newFaction : EFaction):
+	faction = newFaction
+	factionChanged.emit()
+
+func kill():
+	is_dead = true
+	dead.emit()
---
+++ scenes/characters/wololo/Wololo.gd
@@ -0,0 +1,7 @@
+extends Character
+## Wololo class
+##
+## Main player class. handle 'Wololo' and 'Ayoooo' stuffs i guess....
+
+func _physics_process(_delta):
+	super(_delta)
