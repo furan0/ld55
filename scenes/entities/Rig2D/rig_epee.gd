5b5b72376fcdbe13a7cf009439b393710d04a23c
Aune <34304992+Guillemain@users.noreply.github.com>
2024-04-13 20:14:31 +0200

    Rcohe

--- scenes/entities/Rig2D/rig_epee.gd
+++ scenes/entities/Rig2D/rig_epee.gd
@@ -8,10 +8,6 @@ func _ready():
 	$"../../StateMachine/MainState/Alive/Movement/Stopped".state_entered.connect(idle)
 	$"../../StateMachine/MainState/Alive/Status/Attacking/AttackAction/PreAttack".state_entered.connect(preattaque)
 	$"../../StateMachine/MainState/Alive/Status/Attacking/AttackAction/Attack".state_entered.connect(attaque)
-##### Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#if (Input.is_action_just_pressed("down")):
-		#dead()
 
 
 func walk():
