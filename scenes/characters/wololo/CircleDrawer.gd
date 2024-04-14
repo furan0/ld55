5eacbdde46827553097f0827cd6bdf604564b791
Aune <34304992+Guillemain@users.noreply.github.com>
2024-04-14 23:30:00 +0200

    beautirfy circle

--- scenes/characters/wololo/CircleDrawer.gd
+++ scenes/characters/wololo/CircleDrawer.gd
@@ -5,39 +5,31 @@ extends Line2D
 @export var n_points := 20
 @export var radius := 100.0
 
-var to_draw = []
+
+var to_display = 0 :
+	get:
+		return to_display
+	set(value):
+		to_display = value
+		var to_draw = []
+		for i in range(min(to_display,n_points)):
+			var theta = 2 * PI * i/n_points 
+			to_draw += [Vector2(cos(theta),sin(theta)*0.8)*radius]
+		points = PackedVector2Array(to_draw)
+		if to_display >= n_points:
+			closed = true
+		else :
+			closed = false
 
 var drawing = false
 var stop_draw = true
 
 
 func draw_my_circle():
-	drawing = true
-	closed = false
-	to_draw = []
-	for i in range(n_points):
-		var theta = 2 * PI * i/n_points 
-		to_draw += [Vector2(cos(theta),sin(theta)*0.8)*radius]
-		points = PackedVector2Array(to_draw)
-		if i % 5:
-			await get_tree().process_frame
-		#if stop_draw:
-			#stop_draw = false
-			#drawing = false
-			#undraw()
-			#break
-	drawing = false
-	closed = true
+	get_tree().create_tween().tween_property(self,"to_display",n_points,0.2)
 
 func undraw():
-	if len(to_draw) == 0 or drawing:
-		stop_draw = true
-		return
-	closed = false
-	for i in range(n_points):
-		to_draw.pop_back()
-		points = PackedVector2Array(to_draw)
-		await get_tree().process_frame
+	get_tree().create_tween().tween_property(self,"to_display",0,0.2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
