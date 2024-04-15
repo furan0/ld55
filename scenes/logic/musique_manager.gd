6c27cbe7d7913369a4290e2fe7b7daba401ab15a
Aune <34304992+Guillemain@users.noreply.github.com>
2024-04-15 21:03:17 +0200

    looping music

--- scenes/logic/musique_manager.gd
+++ scenes/logic/musique_manager.gd
@@ -2,17 +2,27 @@ extends Node
 
 func fade_stream(stream:AudioStreamPlayer,volume=0,temps=1.0):
 	get_tree().create_tween().tween_property(stream,"volume_db",volume,0.2)
+	$musiquetendue.finished.connect(fade_to_intense)
+	$musiquecalme.finished.connect(fade_to_calme)
 	
+
+func end_calme():
+	pass
+	
+func end_intense():
+	pass
 
 
 func fade_to_calme():
-	fade_stream($musiquetendue,-80.0)
+	if $musiquetendue.playing:
+		fade_stream($musiquetendue,-80.0)
 	fade_stream($musiquecalme,0.0)
 	$musiquecalme.play(0)
 
 func fade_to_intense():
+	if $musiquecalme.playing:
+		fade_stream($musiquecalme,-80.0)
 	fade_stream($musiquetendue,0.0)
-	fade_stream($musiquecalme,-80.0)
 	$musiquetendue.play(0)
 
 func fade_to_lose(character=null):
@@ -32,7 +42,7 @@ func fade_to_victoire(character=null):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_parent().victory.connect(fade_to_victoire)
-	get_tree().create_timer(1.5).timeout.connect(fade_to_calme)
+	get_tree().create_timer(1.5).timeout.connect(fade_to_intense)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
