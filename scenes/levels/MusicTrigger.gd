66decfb3b21db864944edf04e5b51914b831d0d0
furan0 <augustin.delecroix@gmail.com>
2024-04-15 23:27:38 +0200

    Half life trigger added to multi & endless

--- scenes/levels/MusicTrigger.gd
+++ scenes/levels/MusicTrigger.gd
@@ -2,6 +2,8 @@ extends Area2D
 class_name MusicTrigger
 
 @export var isMusicIntense := true
+@export var isHalfLife := false
+var halfLifeTriggered := false
 @export var isOneShot = true
 
 var isDisplaying := false
@@ -12,6 +14,32 @@ signal triggerStarted()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	body_entered.connect(_on_body_entered)
+	if isHalfLife:
+		setupHalfLife.call_deferred()
+
+func setupHalfLife():
+	var player1 = %GameManager.player1
+	if player1 != null:
+		player1.hurt.connect(checkLife)
+	var player2 = %GameManager.player2
+	if player2 != null:
+		player2.hurt.connect(checkLife)
+
+func checkLife():
+	if halfLifeTriggered:
+		return
+		
+	var player1 = %GameManager.player1
+	if player1 != null && player1.health <= player1.MAX_HEALTH / 2:
+		playMusic()
+		halfLifeTriggered = true
+		return
+	
+	var player2 = %GameManager.player2
+	if player2 != null && player2.health <= player2.MAX_HEALTH / 2:
+		playMusic()
+		halfLifeTriggered = true
+		return
 
 func _on_body_entered(body : Node2D):
 	if isDisplaying:
