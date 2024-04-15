f45278428a99d66d291931c54c9a8124d074c01b
Aune <34304992+Guillemain@users.noreply.github.com>
2024-04-15 16:13:41 +0200

    lvl1 complete

--- scenes/levels/minigame/butine.gd
+++ scenes/levels/minigame/butine.gd
@@ -11,17 +11,18 @@ func action_just_press():
 	get_tree().create_tween().tween_property($game/Panel/Abeille,"position",$game/Panel/Abeille.position,randf_range(1.0,1.5))
 
 	if abs($game/Panel/Fleure.position.x - $game/Panel/Abeille.position.x)<tolerance:
-		print("ok")
 		$game/Panel/Abeille.modulate = Color.BISQUE
 		$game/Panel/Abeille.scale = Vector2.ONE * 1.2
 		get_tree().create_tween().tween_property($game/Panel/Fleure,"scale",Vector2.ONE*1.4,0.1)
 		terminateMinigame(true)
 	else:
-		print("ko")
 		$game/Panel/Abeille.modulate = Color.DIM_GRAY
 		$game/Panel/Fleure.modulate = Color.DIM_GRAY
+		$game/Panel.modulate = Color.DIM_GRAY
 		get_tree().create_tween().tween_property($game/Panel/Fleure,"scale",Vector2.ONE*0.4,0.1)
 		terminateMinigame(false)
+		
+		
 func run_bzz():
 	$game/Panel/Abeille.position.x = -165
 	get_tree().create_tween().tween_property($game/Panel/Abeille,"position",Vector2(175,0),randf_range(1.0,1.5)).finished.connect(_endgame)
@@ -30,7 +31,10 @@ func _endgame():
 	if busy:
 		return
 	busy = true
-	print("ko")
+	$game/Panel/Abeille.modulate = Color.DIM_GRAY
+	$game/Panel/Fleure.modulate = Color.DIM_GRAY
+	$game/Panel.modulate = Color.DIM_GRAY
+	get_tree().create_tween().tween_property($game/Panel/Fleure,"scale",Vector2.ONE*0.4,0.1)
 	terminateMinigame(false)
 	
 	
@@ -42,7 +46,7 @@ func runMinigame():
 	busy = false
 	$game/Panel.scale = Vector2(0.3,1.3)
 	get_tree().create_tween().tween_property($game/Panel,"scale",Vector2.ONE,0.1)
-	$game/Panel/Fleure.position.x = randf_range(40,155)
+	$game/Panel/Fleure.position.x = randf_range(40,100)
 	get_tree().create_timer(0.3).timeout.connect(run_bzz)
 	
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
